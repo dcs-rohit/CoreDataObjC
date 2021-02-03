0c1a41c34bb62b075a8ec1577258f55d39b4f7bc
dcs-rohit <rohitchaurasiya.dcs19@vnsgu.ac.in>
2021-02-03 12:50:15 +0530

    Solved auto increment Id.

--- CoreDataExample/ViewController.m
+++ CoreDataExample/ViewController.m
@@ -14,10 +14,63 @@
 
 @implementation ViewController
 NSMutableArray *devices;
+NSNumber *eId;
 - (void)viewDidLoad {
     [super viewDidLoad];
+    eId = [[NSNumber alloc]init];
     _appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+  
+    [self getEmployeeID];
+}
+- (void)viewWillAppear:(BOOL)animated{
+    NSLog(@"showing View Controller");
+    self.txtName.text =@"";
+    self.txtSalary.text =@"";
+    self.txtID.text =@"";
+    [self getEmployeeID];
+}
+- (IBAction)saveButtonClicked:(id)sender {
+    // Create a new managed object
+    NSString *empName = self.txtName.text;
+    NSString *empSalary= self.txtSalary.text;
+    NSString *empID = self.txtID.text;
+    if([empName length] != 0 && [empSalary length] != 0 && [empID length] != 0){
+        
     
+    NSManagedObjectContext *context = [_appDelegate managedObjectContext];
+    NSManagedObject *newEmployee = [NSEntityDescription insertNewObjectForEntityForName:@"Employee" inManagedObjectContext:context];
+    NSNumberFormatter *f = [[NSNumberFormatter alloc] init];
+    f.numberStyle = NSNumberFormatterDecimalStyle;
+    NSNumber *dSalary = [f numberFromString:empSalary];
+    NSNumber *eId = [f numberFromString:empID];
+//    eId = [NSNumber numberWithInt:1];
+//    eId = [NSNumber numberWithInt:[eId intValue]+1];
+    [newEmployee setValue:eId forKey:@"id"];
+    [newEmployee setValue:empName forKey:@"name"];
+    [newEmployee setValue:dSalary forKey:@"salary"];
+    
+    NSError *error = nil;
+    // Save the object to persistent store
+    if (![context save:&error]) {
+        NSLog(@"Can't Save! %@ %@", error, [error localizedDescription]);
+    }
+    
+    EmployeeList *controller = [[EmployeeList alloc]init];
+    [self.navigationController pushViewController:controller animated:YES];
+    }
+    else{
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Alert" message:@"All Required Field." preferredStyle:UIAlertControllerStyleAlert];
+
+        UIAlertAction *ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                                //button click event
+                            }];
+        [alert addAction:ok];
+        [self presentViewController:alert animated:YES completion:nil];
+       
+    }
+}
+
+-(void)getEmployeeID{
     //Setting Employee ID
     NSManagedObjectContext *context = [_appDelegate managedObjectContext];
     NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] initWithEntityName:@"Employee"];
@@ -33,42 +86,8 @@ NSMutableArray *devices;
         f.numberStyle = NSNumberFormatterDecimalStyle;
         NSNumber *eId = [f numberFromString:[NSString stringWithFormat:@"%@" ,[device valueForKey:@"id"]]];
         eId = [NSNumber numberWithInt:[eId intValue] + 1];
-        
-        _txtID.text = [NSString stringWithFormat:@"%@" ,eId];
+        _txtID.text = [NSString stringWithFormat:@"%@" ,eId!=nil ? eId : 0];
         [_txtID setEnabled:NO];
     }
-}
-- (IBAction)saveButtonClicked:(id)sender {
-    // Create a new managed object
-    NSManagedObjectContext *context = [_appDelegate managedObjectContext];
-    //For Saving Data
-    NSManagedObject *newDevice = [NSEntityDescription insertNewObjectForEntityForName:@"Employee" inManagedObjectContext:context];
-    
-    NSString *empName = self.txtName.text;
-    NSString *empSalary= self.txtSalary.text;
-    NSString *empID = self.txtID.text;
-    
-    NSNumberFormatter *f = [[NSNumberFormatter alloc] init];
-    f.numberStyle = NSNumberFormatterDecimalStyle;
-    NSNumber *dSalary = [f numberFromString:empSalary];
-    NSNumber *eId = [f numberFromString:empID];
-//    eId = [NSNumber numberWithInt:1];
-//    eId = [NSNumber numberWithInt:[eId intValue]+1];
-    
-    
-    
-    [newDevice setValue:eId forKey:@"id"];
-    [newDevice setValue:empName forKey:@"name"];
-    [newDevice setValue:dSalary forKey:@"salary"];
-    
-    NSError *error = nil;
-    // Save the object to persistent store
-    if (![context save:&error]) {
-        NSLog(@"Can't Save! %@ %@", error, [error localizedDescription]);
-    }
-    
-    EmployeeList *controller = [[EmployeeList alloc]init];
-    [self.navigationController pushViewController:controller animated:YES];
-    
 }
 @end
